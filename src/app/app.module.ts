57840c91aa64180ad7742ec11ca655289df3329c
dellydel <dlvhall@gmail.com>
2019-02-12 22:21:18 -0600

    removed unused module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material';
-import { BrowserAnimationsModule, ComponentPageHeader } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
