2ad5f83969dd9caf9f5153e2039951a70dc0c876
Delvin Hall <dlvhall@gmail.com>
2019-11-08 14:40:49 -0600

    React

---
+++ journo/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        
+      </header>
+      <div>JOURNO</div>
+    </div>
+  );
+}
+
+export default App;
