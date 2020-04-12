6bde21e78204138eb6680dda84c65d3fc853e79e
Delvin <dlvhall@gmail.com>
2020-04-11 19:34:35 -0500

    Updated helper class / es6 module; Renamed Journos

--- journos/src/index.js
+++ journos/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Journos from './components/Journos';
+import JournosMain from './components/JournosMain';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
         <div>
             <Route exact path={"/"} component={App} />
-            <Route path={"/journos"} component={Journos} />
+            <Route path={"/journos"} component={JournosMain} />
         </div>
     </BrowserRouter>, document.getElementById('root'));
 
--- journos/src/journos.dev.js
+++ journos/src/journos.dev.js
@@ -1,66 +1,61 @@
-function showPopup() {
-    alert('testing');
-}
 
-function getText() {
-    var selectedText = getSelectedText();
-    if (selectedText.length) {
-        alert(selectedText);
+export default class {
+    static getText() {
+        var selectedText = ""
+        if (window.getSelection) {
+            selectedText = window.getSelection().toString();
+        }
+        else if (document.getSelection) {
+            selectedText = document.getSelection();
+        }
+        else if (document.selection) {
+            selectedText =
+                document.selection.createRange().text;
+        }
+        if (selectedText.length) {
+            alert(selectedText);
+        }
     }
-}
 
-function formatSelectedText(color) {
-    var selectedText = window.getSelection();
-    let range = selectedText.getRangeAt(0);
+    static formatSelectedText(color) {
+        var selectedText = window.getSelection();
+        let range = selectedText.getRangeAt(0);
 
-    let newNode = document.createElement('span');
-    newNode.style.color = color;
-    try {
-        range.surroundContents(newNode);
-    } catch (e) { alert(e) }
-}
-
-function makeParagraphLink() {
-    var selectedText = window.getSelection();
-    let range = selectedText.getRangeAt(0);
-    let anchorTag = document.createElement('a');
-    anchorTag.href = '';
-    anchorTag.title = 'updated ' + new Date().toLocaleDateString();
-
-    var el = range.commonAncestorContainer;
-
-    el.parentNode.insertBefore(anchorTag, el);
-    anchorTag.appendChild(el);
-    //el.classList.add('challengeLink');
-}
-
-function makeSelectionLink() {
-    var selectedText = window.getSelection();
-   let range = selectedText.getRangeAt(0);
-
-    let anchorTag = document.createElement('a');
-    anchorTag.href = '';
-    anchorTag.title = 'updated ' + new Date().toLocaleDateString();
-    try {
-        range.surroundContents(anchorTag);
-    } catch (e) { alert(e) }
-}
-
-function formatText(text) {
-    alert('text formatted: ' + text);
-}
-
-function getSelectedText() {
-    var selectedText = ""
-    if (window.getSelection) {
-        selectedText = window.getSelection().toString();
+        let newNode = document.createElement('span');
+        newNode.style.color = color;
+        try {
+            range.surroundContents(newNode);
+        } catch (e) { alert(e) }
     }
-    else if (document.getSelection) {
-        selectedText = document.getSelection();
+
+    static makeParagraphLink() {
+        var selectedText = window.getSelection();
+        let range = selectedText.getRangeAt(0);
+        let anchorTag = document.createElement('a');
+        anchorTag.href = '';
+        anchorTag.title = 'updated ' + new Date().toLocaleDateString();
+
+        var el = range.commonAncestorContainer;
+
+        el.parentNode.insertBefore(anchorTag, el);
+        anchorTag.appendChild(el);
     }
-    else if (document.selection) {
-        selectedText =
-            document.selection.createRange().text;
+
+    static makeSelectionLink() {
+        var selectedText = window.getSelection();
+        let range = selectedText.getRangeAt(0);
+
+        let anchorTag = document.createElement('a');
+        anchorTag.href = '';
+        anchorTag.title = 'updated ' + new Date().toLocaleDateString();
+        try {
+            range.surroundContents(anchorTag);
+        } catch (e) { alert(e) }
     }
-    return selectedText;
+
+    static formatText(text) {
+        alert('text formatted: ' + text);
+    }
+
 }
+
